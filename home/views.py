642c31ba3b9ebcbb6e10c4b86628fad44125c3df
NOLLYVILLE <frisk.harrison@yahoo.com>
2021-06-16 11:07:32 +0100

    first commit

---
+++ home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import *
+# Create your views here.
+
+def index(request):
+    type = Type.objects.all()
+
+    context = {
+        'jibola':type
+    }
+    return render(request, 'index.html',context)
+    # return HttpResponse('App installed')
+
+def food(request,id):
+    prep = Preparation.objects.filter(type_id=id)   
+
+    context = {
+        'barth':prep
+    }
+
+    return render(request, 'food.html', context)
+
+
+def details(request):
+    detail = Preparation.objects.get(pk=id)
+
+
+    context={
+        'detail':detail
+    }
+
+    return render(request, 'detail.html', context)
